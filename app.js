90f7d123eb2a876b5908e818a47ed7b7722edc64
“Genaro <ramoon1996@hotmail.com>
2021-04-17 08:47:32 +0200

    done 1-4

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('home');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => res.render('beers', beersFromApi))
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => 
+    res.render('random-beer', { responseFromAPI }))
+  .catch(error => console.log(error));
+});
+
+app.listen(5000, () => console.log('🏃‍ on port 5000'));
